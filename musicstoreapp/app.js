bf42bc3786db0282425a1fe55e0b2222d3aeb994
carlosalvarezrabano <carlos.arov@hotmail.com>
2022-03-26 13:34:43 +0100

    SDI-2122-902-7.3-División de las rutas en módulos, peticiones GET y parámetros.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -6,17 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+require("./routes/songs.js")(app);
 
 var app = express();
-
-app.get("/songs", function (req, res) {
-  console.log("depurar aqui")
-  res.send("Lista de canciones")
-});
-app.get("/singers", function (req, res) {
-  console.log("depurar aqui")
-  res.send("Lista de cantantes")
-});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
